ba78c97e37eca484406e30ce9d7611844aa05bb0
krishnapriyapadikkal <krishnapriyapadikkal.mec@gmail.com>
2017-05-26 18:50:08 +0000

    url with offset parameter

--- getdata2.php
+++ getdata2.php
@@ -20,41 +20,42 @@ while (true) {
   
 
     if (_iscurl()){
-        $url = "https://api.telegram.org/bot391062433:AAE7lq0n8rIHBPKgqYiuzKELeRlmlBfcYd8/getUpdates";    
+        $url = "https://api.telegram.org/bot391062433:AAE7lq0n8rIHBPKgqYiuzKELeRlmlBfcYd8/getUpdates?offset=";
+	$id=755706114;
+	
+	$offset=$url.$id;
+	  
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);                               
+        curl_setopt($ch, CURLOPT_URL, $offset);                               
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $output = curl_exec($ch);
         curl_close($ch);
         
         $x = json_decode($output, true);
         $len=count($x["result"]);
-       
+	
+
+	
+	
 	$chat_id=0;
 	 for($i=0;$i<$len+1;$i++){
 
 	$chat_id=$x["result"][$i]["message"]["chat"]["id"];
 	/*if(array_key_exists(["result"][$i],$chat_id=$x["result"][$i]["message"]["chat"]["id"]))*/	
-
 	echo $chat_id;
-	
         $msg=$x["result"][$i]["message"]["text"];
-/*
-	if(array_key_exists($i,$msg))	
-*/
-	echo $msg;
-      
-        
+/*	if(array_key_exists($i,$msg))	
+*/	echo $msg;
+    
+        $id=$id+1;
 
-           try {
-
-           $pdo->beginTransaction();
-           $stmt = $pdo->prepare("INSERT INTO askme (chat_id,search) VALUES (?,?)");
+       try {
+       $pdo->beginTransaction();
+       $stmt = $pdo->prepare("INSERT INTO askme (chat_id,search) VALUES (?,?)");
           $stmt->execute([$chat_id,$msg]);
            $pdo->commit();
             }
         catch (Exception $e){
-   
            $pdo->rollback();
            throw $e;
            }               
